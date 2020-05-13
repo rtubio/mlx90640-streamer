846cff4d1efa30573f84dce8ffd977d6e49eaed1
Ricardo Tubio-Pardavila <rtpardavila@gmail.com>
2020-05-13 19:04:06 +0900

    @receiver.sh: added extra log

--- conf/scripts/receiver.sh
+++ conf/scripts/receiver.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+BLOCKSIZE=2304
 PORT=5000
 
-logger "Launching streamer to <localhost:$PORT>"
+logger "Launching streamer to <localhost:$PORT>, blocksize = $BLOCKSIZE"
 gst-launch-1.0 udpsrc blocksize=2304 port=$PORT ! rawvideoparse use-sink-caps=false width=32 height=24 format=rgb framerate=16/1 ! videoconvert ! videoscale ! video/x-raw,width=640,height=480 ! autovideosink
