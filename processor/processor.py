064b705b8e0fc6a27f786fc35a0222adc77f8285
Ricardo Tubio-Pardavila <rtpardavila@gmail.com>
2020-05-14 05:19:20 +0900

    Started writing the data processor for the binary MLX90640 data.

--- processor/processor.py
+++ processor/processor.py
@@ -3,51 +3,68 @@ This is the processor that digests the raw data from MLX90640 and analyzes it.
 """
 
 import argparse
+import numpy as np
 
-from xpython.common import logger
+from xpython.common import files, logger
 
 
 class MLX90640RawFrame(logger.LoggingClass):
     """RAW binary frame straight out from MLX90640
-    This is an array of type uint16_t, with a fixed length of 834 elements
+    This is an array of floats, with 32x24 elements (MLX90640 pixels)
     """
 
-    LENGTH = 834
+    PIXELS_X        = 32
+    PIXELS_Y        = 24
+    PIXELS_FRAME    = PIXELS_X * PIXELS_Y
 
-    def __init__(self, fps, file_path):
+    def __init__(self, frame):
+        """Default constructor
+        data - array with the pixel data
+        """
+        self.frame = frame
+
+
+class MLX90640RawDataProcessor(logger.LoggingClass):
+    """RAW binary data file processor
+    This class processes the raw binary data as gathered from MLX90640.
+    Frames are suppossed to be stored sequentially within the file, at a rate of FPS
+    frames per second. The value of FPS determines the time distance in between frames.
+    """
+
+    def __init__(self, fps, raw_filepath):
         """Default constructor
         fps - frames per second, necessary to calculate the timeline
-        file_path - path to the binary file with the RAW frames
+        raw_filepath - path to the binary file with the RAW frames
         """
         self.fps = fps
-        self.file_path = file_path
+        self.raw_filepath = raw_filepath
+        self.sequence = []
 
-
-class MLX90640RawFrameProcessor(logger.LoggingClass):
+    def process(self):
+        """
+        This method processes the given file, where the RAW data is supposed to be stored.
+        """
+        with open(self.raw_filepath, mode='r') as f:
+            pass
 
     @staticmethod
     def create(argv):
         # Basic class static factory method.
 
-        parser = argparse.ArgumentParser(description="Updates old JSON file with new JSON file")
+        parser = argparse.ArgumentParser(description="Process a file with raw data from MLX90640")
         parser.add_argument(
-            "-o", "--old",
-            type=files.is_writable_file, metavar="FILE", required=True,
-            help="Path to the old JSON file to be updated"
+            "-r", "--raw_file",
+            type=files.is_readable_file, metavar="FILE", required=True,
+            help="Path to the binary file to be processed"
         )
         parser.add_argument(
-            "-n", "--new",
-            type=files.is_writable_file, metavar="FILE", required=True,
-            help="Path to the new JSON file to use to update the old one"
+            "-f", "--fps", type=int, required=True,
+            help="Frames per second, required for timing calculation"
         )
-        parser.add_argument(
-            "-b", "--backup",
-            default=False, action="store_true", required=False,
-            help="Enables creating a backup of the old file"
-        )
+
         args = parser.parse_args(argv)
-        return MLX90640RawFrameProcessor(args.old, args.new, backup=args.backup)
+        return MLX90640RawDataProcessor(args.fps, args.raw_file)
 
 
 if __name__ == "__main__":
-    print(f"Test, {__name__}")
+    processor = MLX90640RawDataProcessor.create(sys.argv[1:])
