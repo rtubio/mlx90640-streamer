604d8ed08d8be1ac84db31da6bbaa226c399de34
Ricardo Tubio-Pardavila <rtpardavila@gmail.com>
2020-05-13 19:53:53 +0900

    Modified receiver for logging and displaying

--- conf/scripts/receiver.sh
+++ conf/scripts/receiver.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+DATALOG=/tmp/datalog.bin
 BLOCKSIZE=2304
-PORT=5000
+PORT_RX=5000
+PORT_FORWARD=5001
 
 logger "Launching streamer to <localhost:$PORT>, blocksize = $BLOCKSIZE"
-gst-launch-1.0 udpsrc blocksize=2304 port=$PORT ! rawvideoparse use-sink-caps=false width=32 height=24 format=rgb framerate=16/1 ! videoconvert ! videoscale ! video/x-raw,width=640,height=480 ! autovideosink
+
+# nc -lp "$PORT" | tee "$DATALOG" | nc localhost "$PORT_FORWARD"
+nc -ulp $PORT_RX | tee "$DATALOG" | socat stdin udp:127.0.0.1:$PORT_FORWARD &
+
+gst-launch-1.0 udpsrc blocksize=2304 port=$PORT_FORWARD ! rawvideoparse use-sink-caps=false width=32 height=24 format=rgb framerate=16/1 ! videoconvert ! videoscale ! video/x-raw,width=640,height=480 ! autovideosink
