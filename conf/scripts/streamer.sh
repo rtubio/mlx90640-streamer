0ce18c9e04ed0c706956403f9b8864037bdff9a5
Ricardo Tubio-Pardavila <rtpardavila@gmail.com>
2020-05-18 17:01:13 +0900

    Modified streamer script to send stderr output data to a binary temporary file.

--- conf/scripts/streamer.sh
+++ conf/scripts/streamer.sh
@@ -6,5 +6,5 @@ logger "[info, $0] Launching streamer to <$HOST:$PORT> with fps ($FPS)"
 
 [[ -f "$DATASETBIN" ]] && rm -f "$DATASETBIN" && logger "[warn, $0] <$DATASETBIN> removed"
 
-/usr/local/bin/mlx90640-streamer $FPS | tee "$DATASETBIN" | \
+/usr/local/bin/mlx90640-streamer $FPS "2>$DATASETBIN" | \
   gst-launch-1.0 fdsrc blocksize=2304 ! udpsink host=$HOST port=$PORT
