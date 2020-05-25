6836179db6e16e72d948e4388ec8bf3a52894dcf
Ricardo Tubio-Pardavila <rtpardavila@gmail.com>
2020-05-25 18:09:45 +0900

    Simple countdown script added.

---
+++ conf/scripts/countdown.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Simple countdown script to monitor the time left for a sequential process.
+#
+# rtpardavila[at]gmail[dot]com
+
+counter="$1"
+
+while [ $counter -gt 0 ]
+do
+  echo -n "$counter "
+  ((counter--))
+  sleep 1
+done
