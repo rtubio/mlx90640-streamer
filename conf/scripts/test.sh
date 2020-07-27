187f31c3bf4037010d6db30dcfac5d34848a2fac
Ricardo Tubio-Pardavila <rtpardavila@gmail.com>
2020-07-27 15:56:57 +0900

    Automated test with bash script

---
+++ conf/scripts/test.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# This script executes a test using the MLX90640 installed service.
+#
+# ricardo[at]karbontek.co.jp
+
+[[ $# -ne 3 ]] && {
+  echo "Need to specify the following 3 arguments to call this script:"
+  echo "> USER : name of the user in the remote computer where the data is to be copied to"
+  echo "> HOST : name of the remote host where the data is to be copied to"
+  echo "> MAX  : number of seconds that the test lasts"
+  exit -1
+}
+
+USER="$1"
+HOST="$2"
+LOCAL_DATABIN="/tmp/databin.bin"
+REMOTE_DATABIN="~/databin.raw"
+
+left=0
+max="$3"     # default time to wait for the test to end
+
+sudo service mlx90640 restart || {
+  echo "Could not restart/start the MLX90640 service, please check."
+  exit -1
+}
+
+echo "Countdown until the test ends..."
+for counter in $( seq 0 1 $max )
+do
+  let "left=max-counter"
+  echo -n "$left "
+  sleep 1
+done
+echo "Test ended, stopping services and collecting data..."
+
+sudo service mlx90640 stop || {
+  echo "Could not stop the MLX90640 service, please check."
+  exit -1
+}
+
+scp "$LOCAL_DATABIN" $USER@$HOST:~/"$REMOTE_DATABIN" || {
+  echo "Could not copy <$LOCAL_DATABIN> to <$REMOTE_DATABIN> as <$USER> on <$HOST>"
+  exit -1
+}
+
+echo "Test executed succesffully"
